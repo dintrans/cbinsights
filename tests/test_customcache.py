c3fa6e281a919dd73855f8308f8780a9643ea2f2
Gabriel Dintrans <dintrans.g@gmail.com>
2021-06-11 17:43:02 -0400

    tests migrated from unittest to pytest

--- tests/test_customcache.py
+++ tests/test_customcache.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 from src.CustomTtlCache import CustomTTLCache
 
 
@@ -21,132 +21,130 @@ class CustomTTLTestCache(CustomTTLCache):
         CustomTTLCache.__init__(self, maxsize, default_ttl, eviction_policy, timer=Timer(), **kwargs)
 
 
-class CustomTTLCacheTest(unittest.TestCase):
-
-    Cache = CustomTTLTestCache
-
-    def test_ttl(self):
-        cache = CustomTTLCache(2, 1, 'REJECT', timer=Timer())
-        self.assertEqual(0, cache.timer())
-        self.assertEqual(1, cache.ttl)
-
-        cache[1] = 1
-        self.assertEqual({1}, set(cache))
-        self.assertEqual(1, len(cache))
-        self.assertEqual(1, cache[1])
-
-        cache.timer.tick()
-        self.assertEqual({1}, set(cache))
-        self.assertEqual(1, len(cache))
-        self.assertEqual(1, cache[1])
-        cache[2] = 2
-        self.assertEqual({1, 2}, set(cache))
-        self.assertEqual(2, len(cache))
-        self.assertEqual(1, cache[1])
-        self.assertEqual(2, cache[2])
-
-        cache.timer.tick()
-        self.assertEqual({2}, set(cache))
-        self.assertEqual(1, len(cache))
-        self.assertNotIn(1, cache)
-        self.assertEqual(2, cache[2])
-
-        cache[3] = 3
-        self.assertEqual({2, 3}, set(cache))
-        self.assertEqual(2, len(cache))
-        self.assertNotIn(1, cache)
-        self.assertEqual(2, cache[2])
-        self.assertEqual(3, cache[3])
-
-        cache.timer.tick()
-        self.assertEqual({3}, set(cache))
-        self.assertEqual(1, len(cache))
-        self.assertNotIn(1, cache)
-        self.assertNotIn(2, cache)
-        self.assertEqual(3, cache[3])
-
-        cache.timer.tick()
-        self.assertEqual(set(), set(cache))
-        self.assertEqual(0, len(cache))
-        self.assertNotIn(1, cache)
-        self.assertNotIn(2, cache)
-        self.assertNotIn(3, cache)
-
-        with self.assertRaises(KeyError):
-            del cache[1]
-
-    def test_ttl_expire(self):
-        cache = CustomTTLCache(3, 2, 'REJECT', timer=Timer())
-        with cache.timer as time:
-            self.assertEqual(time, cache.timer())
-        self.assertEqual(2, cache.ttl)
-
-        cache[1] = 1
-        cache.timer.tick()
-        cache[2] = 2
-        cache.timer.tick()
-        cache[3] = 3
-        self.assertEqual(2, cache.timer())
-
-        self.assertEqual({1, 2, 3}, set(cache))
-        self.assertEqual(3, len(cache))
-        self.assertEqual(1, cache[1])
-        self.assertEqual(2, cache[2])
-        self.assertEqual(3, cache[3])
-
-        cache.expire()
-        self.assertEqual({1, 2, 3}, set(cache))
-        self.assertEqual(3, len(cache))
-        self.assertEqual(1, cache[1])
-        self.assertEqual(2, cache[2])
-        self.assertEqual(3, cache[3])
-
-        cache.expire(3)
-        self.assertEqual({2, 3}, set(cache))
-        self.assertEqual(2, len(cache))
-        self.assertNotIn(1, cache)
-        self.assertEqual(2, cache[2])
-        self.assertEqual(3, cache[3])
-
-        cache.expire(4)
-        self.assertEqual({3}, set(cache))
-        self.assertEqual(1, len(cache))
-        self.assertNotIn(1, cache)
-        self.assertNotIn(2, cache)
-        self.assertEqual(3, cache[3])
-
-        cache.expire(5)
-        self.assertEqual(set(), set(cache))
-        self.assertEqual(0, len(cache))
-        self.assertNotIn(1, cache)
-        self.assertNotIn(2, cache)
-        self.assertNotIn(3, cache)
-
-    def test_ttl_atomic(self):
-        cache = CustomTTLCache(1, 1, 'REJECT', timer=Timer(auto=True))
-        cache[1] = 1
-        self.assertEqual(1, cache[1])
-        cache[1] = 1
-        self.assertEqual(1, cache.get(1))
-        cache[1] = 1
-        self.assertEqual(1, cache.pop(1))
-        cache[1] = 1
-        self.assertEqual(1, cache.setdefault(1))
-        cache[1] = 1
-        cache.clear()
-        self.assertEqual(0, len(cache))
-
-    def test_ttl_tuple_key(self):
-        cache = CustomTTLCache(1, 0, 'REJECT', timer=Timer())
-        self.assertEqual(0, cache.ttl)
-
-        cache[(1, 2, 3)] = 42
-        self.assertEqual(42, cache[(1, 2, 3)])
-        cache.timer.tick()
-        with self.assertRaises(KeyError):
-            cache[(1, 2, 3)]
-        self.assertNotIn((1, 2, 3), cache)
+Cache = CustomTTLTestCache
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_ttl():
+    cache = CustomTTLCache(2, 1, 'REJECT', timer=Timer())
+    assert 0 == cache.timer()
+    assert 1 == cache.ttl
+
+    cache[1] = 1
+    assert {1} == set(cache)
+    assert 1 == len(cache)
+    assert 1 == cache[1]
+
+    cache.timer.tick()
+    assert {1} == set(cache)
+    assert 1 == len(cache)
+    assert 1 == cache[1]
+    cache[2] = 2
+    assert {1, 2} == set(cache)
+    assert 2 == len(cache)
+    assert 1 == cache[1]
+    assert 2 == cache[2]
+
+    cache.timer.tick()
+    assert {2} == set(cache)
+    assert 1, len(cache)
+    assert 1 not in cache
+    assert 2 == cache[2]
+
+    cache[3] = 3
+    assert {2, 3} == set(cache)
+    assert 2 == len(cache)
+    assert 1 not in cache
+    assert 2 == cache[2]
+    assert 3 == cache[3]
+
+    cache.timer.tick()
+    assert {3} == set(cache)
+    assert 1 == len(cache)
+    assert 1 not in cache
+    assert 2 not in cache
+    assert 3 == cache[3]
+
+    cache.timer.tick()
+    assert set() == set(cache)
+    assert 0 == len(cache)
+    assert 1 not in cache
+    assert 2 not in cache
+    assert 3 not in cache
+
+    with pytest.raises(KeyError):
+        del cache[1]
+
+
+def test_ttl_expire():
+    cache = CustomTTLCache(3, 2, 'REJECT', timer=Timer())
+    with cache.timer as time:
+        assert time == cache.timer()
+    assert 2 == cache.ttl
+
+    cache[1] = 1
+    cache.timer.tick()
+    cache[2] = 2
+    cache.timer.tick()
+    cache[3] = 3
+    assert 2 == cache.timer()
+
+    assert {1, 2, 3} == set(cache)
+    assert 3 == len(cache)
+    assert 1 == cache[1]
+    assert 2 == cache[2]
+    assert 3 == cache[3]
+
+    cache.expire()
+    assert {1, 2, 3} == set(cache)
+    assert 3, len(cache)
+    assert 1 == cache[1]
+    assert 2 == cache[2]
+    assert 3 == cache[3]
+
+    cache.expire(3)
+    assert {2, 3} == set(cache)
+    assert 2 == len(cache)
+    assert 1 not in cache
+    assert 2 == cache[2]
+    assert 3 == cache[3]
+
+    cache.expire(4)
+    assert {3} == set(cache)
+    assert 1 == len(cache)
+    assert 1 not in cache
+    assert 2 not in cache
+    assert 3 == cache[3]
+
+    cache.expire(5)
+    assert set() == set(cache)
+    assert 0 == len(cache)
+    assert 1 not in cache
+    assert 2 not in cache
+    assert 3 not in cache
+
+
+def test_ttl_atomic():
+    cache = CustomTTLCache(1, 1, 'REJECT', timer=Timer(auto=True))
+    cache[1] = 1
+    assert 1 == cache[1]
+    cache[1] = 1
+    assert 1 == cache.get(1)
+    cache[1] = 1
+    assert 1 == cache.pop(1)
+    cache[1] = 1
+    assert 1 == cache.setdefault(1)
+    cache[1] = 1
+    cache.clear()
+    assert 0 == len(cache)
+
+
+def test_ttl_tuple_key():
+    cache = CustomTTLCache(1, 0, 'REJECT', timer=Timer())
+    assert 0 == cache.ttl
+
+    cache[(1, 2, 3)] = 42
+    assert 42 == cache[(1, 2, 3)]
+    cache.timer.tick()
+    with pytest.raises(KeyError):
+        cache[(1, 2, 3)]
+    assert (1, 2, 3) not in cache
